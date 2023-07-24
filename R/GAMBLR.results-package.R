c840015bc4c4ce0621a88f04f32a7560fd760c9d
vladimirsouza <vladimirbcs@gmail.com>
2023-07-24 02:01:33 -0700

    Fix some dependency issues

---
+++ R/GAMBLR.results-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats end
+#' @importFrom stats start
+#' @importFrom utils head
+#' @importFrom utils read.table
+#' @importFrom utils tail
+## usethis namespace: end
+NULL
