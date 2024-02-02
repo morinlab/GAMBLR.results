4b69271b089dde958098b65e05c8dff041739c63
Kdreval <k.dreval@gmail.com>
2024-02-01 16:50:37 -0800

    bug fix: fix outdated id_ease return style

--- R/calc_mutation_frequency_bin_region.R
+++ R/calc_mutation_frequency_bin_region.R
@@ -80,8 +80,8 @@ calc_mutation_frequency_bin_region <- function(region,
     these_sample_ids,
     this_seq_type
   )
-  metadata <- get_meta$this_metadata
-  these_sample_ids <- get_meta$these_samples
+  metadata <- get_meta
+  these_sample_ids <- get_meta$sample_id %>% unique
 
 
   if (
