34c75885137e6e1210c05efbaf46a4cd5b281217
rdmorin <rdmorin@sfu.ca>
2025-01-31 08:17:45 -0800

    getting there

--- R/get_cn_segments.R
+++ R/get_cn_segments.R
@@ -58,9 +58,6 @@ create_seg_data <- function(seg_df, genome_build) {
 #'                                        with_chr_prefix = TRUE)
 #'
 get_cn_segments = function(region,
-                           chromosome,
-                           qstart,
-                           qend,
                            projection = "grch37",
                            weighted_average=FALSE,
                            seg_data,
@@ -70,137 +67,73 @@ get_cn_segments = function(region,
                            from_flatfile = TRUE,
                            these_samples_metadata){
 
-  
+  if(!missing(seg_data) & !missing(region)){
+    #for backwards compatability, this function can still do the processing of pre-loaded seg_data
+    print("calling helper function")
+    seg_data = process_cn_segments_by_region(seg_data=seg_data,
+                                             region=region,
+                                             streamlined=streamlined,
+                                             weighted_average=weighted_average)
+    return(seg_data)
+    
+  }
   if(!missing(this_seq_type)){
     message("this_seq_type has been deprecated in get_cn_segments")
   }
-  if(missing(these_samples_metadata) & missing(seg_data)){
+  if(missing(these_samples_metadata)){
       message("no metadata provided, will get segments for every genome and capture sample")
       these_samples_metadata = get_gambl_metadata() %>% filter(seq_type %in% c("genome","capture"))
       seq_type = pull(these_samples_metadata,seq_type) %>% unique()
   }else if(!missing(these_samples_metadata)){
     seq_type = pull(these_samples_metadata,seq_type) %>% unique()
   }
-  
-  
-  #perform wrangling on the region to have it in the correct format.
-  if(!missing(region)){
-    region = gsub(",", "", region)
-    split_chunks = unlist(strsplit(region, ":"))
-    chromosome = split_chunks[1]
-    startend = unlist(strsplit(split_chunks[2], "-"))
-    qstart = startend[1]
-    qend = startend[2]
-  }
-  if(!missing(chromosome)){
-    #deal with chr prefixes for region, based on selected genome projection.
-    if(projection == "grch37"){
-      if(grepl("chr", chromosome)){
-        chromosome = gsub("chr", "", chromosome)
-      }
-    }else{
-      if(!grepl("chr", chromosome)){
-        chromosome = paste0("chr", chromosome)
-      }
-    }
-  }
-  
-  if(!missing(qstart)){
-    qstart = as.numeric(qstart)
-  }
-  #enforce data type for qend and qstart coordiantes.
-  if(!missing(qend)){
-    qend = as.numeric(qend)
-  }
-  
-  if(!missing(seg_data)){
-
-    #work directly from the data provided
-    all_segs = 
-      dplyr::filter(seg_data, (chrom == chromosome & start >= qstart & start <= qend)|
-                               (chrom == chromosome & end > qstart & end < qend)|
-                               (chrom == chromosome & end > qend & start < qstart)) %>%
-      mutate(start=ifelse(start < qstart,qstart,start),end=ifelse(end>qend,qend,end)) %>% 
-      mutate(length=end-start)
-    
-    all_segs = all_segs %>% mutate(CN_L = length * CN,logr_L = length*log.ratio) 
-    if(weighted_average){
-      all_segs = all_segs %>% 
-        group_by(ID) %>%
-        summarise(total_L = sum(length), log.ratio = sum(logr_L)/sum(length), 
-                  CN = sum(CN_L)/sum(length)) %>% ungroup() 
-      
-    }else{
-      all_segs = dplyr::mutate(all_segs, CN = round(2*2^log.ratio))
-    }
-    if(streamlined){
-      all_segs = dplyr::select(all_segs, ID, CN)
-    }
-    return(all_segs)
-    
-  }else{
-    cnv_flatfile_template = GAMBLR.helpers::check_config_value(config::get("results_flatfiles")$cnv_combined$icgc_dart)
+  cnv_flatfile_template = GAMBLR.helpers::check_config_value(config::get("results_flatfiles")$cnv_combined$icgc_dart)
+  cnv_path =  glue::glue(cnv_flatfile_template)
+  full_cnv_path =  paste0(GAMBLR.helpers::check_config_value(config::get("project_base")), cnv_path)
+  print(full_cnv_path)
+    #check permissions to ICGC data.
+  permissions = file.access(full_cnv_path[1], 4)
+  if(permissions == -1){
+    message(paste("failed loading from",full_cnv_path[1]))
+    message("restricting to non-ICGC data")
+    cnv_flatfile_template = GAMBLR.helpers::check_config_value(config::get("results_flatfiles")$cnv_combined$gambl)
     cnv_path =  glue::glue(cnv_flatfile_template)
     full_cnv_path =  paste0(GAMBLR.helpers::check_config_value(config::get("project_base")), cnv_path)
-    print(full_cnv_path)
-    #check permissions to ICGC data.
-    permissions = file.access(full_cnv_path[1], 4)
-    if(permissions == -1){
-      message(paste("failed loading from",full_cnv_path[1]))
-      message("restricting to non-ICGC data")
-      cnv_flatfile_template = GAMBLR.helpers::check_config_value(config::get("results_flatfiles")$cnv_combined$gambl)
-      cnv_path =  glue::glue(cnv_flatfile_template)
-      full_cnv_path =  paste0(GAMBLR.helpers::check_config_value(config::get("project_base")), cnv_path)
-    }
-
-    #check for missingness.
-    if(!file.exists(full_cnv_path[1])){
-      print(paste("missing: ", full_cnv_path[1]))
-      message("Cannot find file locally. If working remotely, perhaps you forgot to load your config (see below) or sync your files?")
-      message('Sys.setenv(R_CONFIG_ACTIVE = "remote")')
-    }
-    df_list <- suppressMessages(map(full_cnv_path, read_tsv))
-    all_segs = bind_rows(df_list) %>%  as.data.frame()
-    if(missing(qstart) & missing(qend) & missing(region)){
-      if(missing(chromosome)){
-
-      }else{
-        all_segs = all_segs %>%
-          dplyr::filter(chrom == chromosome) 
-      }
-    }
-    else{
-      all_segs = all_segs %>%
-        dplyr::filter((chrom == chromosome & start <= qstart & end >= qend) | (chrom == chromosome & start >= qstart & end <= qend)) 
-    }
-    
-    all_segs = dplyr::mutate(all_segs, CN = round(2*2^log.ratio))
   }
 
-  #mutate CN states.
+  #check for missingness.
+  if(!file.exists(full_cnv_path[1])){
+    print(paste("missing: ", full_cnv_path[1]))
+    message("Cannot find file locally. If working remotely, perhaps you forgot to load your config (see below) or sync your files?")
+    message('Sys.setenv(R_CONFIG_ACTIVE = "remote")')
+  }
+  df_list <- suppressMessages(map(full_cnv_path, read_tsv))
   
-
-  #deal with chr prefixes
-  if(!with_chr_prefix){
+  all_segs = bind_rows(df_list) %>%  as.data.frame()
+  
+  all_segs = dplyr::mutate(all_segs, CN = round(2*2^log.ratio))
+  
+  #ensure chr prefixes are there when necessary 
+  if(projection=="grch37"){
     if(all(str_detect(all_segs$chrom, "chr"))){
       all_segs = all_segs %>%
         dplyr::mutate(chrom = gsub("chr", "", chrom))
     }
   }else{
+    print("add prefix")
     if(all(!str_detect(all_segs$chrom, "chr"))){
       all_segs = all_segs %>%
         dplyr::mutate(chrom = paste0("chr", chrom))
     }
   }
 
-  #subset to only a few columns with streamlined = TRUE.
-  if(streamlined){
-    all_segs = dplyr::select(all_segs, ID, CN)
-  }
+
   if(!missing(these_samples_metadata)){
     these_samples = pull(these_samples_metadata,sample_id)
     all_segs = dplyr::filter(all_segs,ID %in% these_samples)
   }
+
+  print(head(all_segs))
   #return S3 class with CN segments and genome_build 
   print(class(all_segs))
   all_segs = create_seg_data(all_segs,projection)
--- R/get_cn_states.R
+++ R/get_cn_states.R
@@ -8,7 +8,7 @@
 
 #' @param strategy The general strategy to define regions. Available options are: 'custom_regions','auto_split','cytobands','GISTIC' 
 #' @param regions Required when strategy is set to 'custom_regions'. A data frame in bed-like format or a vector of regions in the format "chrom:start-end"
-#' @param these_samples_metadata Optional metadata table to auto-subset the data to samples in that table before returning. If missing, the result will include a row for every sample in seg_data.
+#' @param these_samples_metadata Optional (but highly recommended) metadata table to auto-subset the data to samples in that table before returning. If missing, the result will include a row for every sample in seg_data.
 #' @param n_bins_split Split genome into N equally sized bins
 #' @param use_cytoband_name Use cytoband names instead of region names, e.g p36.33.
 #' @param missing_data_as_diploid Fill in any sample/region combinations with missing data as diploid (e.g., CN state like 2). Default is FALSE.
@@ -20,9 +20,16 @@
 #' @export
 #'
 #' @examples
-#' #basic usage, generic lymphoma gene list
-#' cn_matrix = get_cn_states(regions_bed=GAMBLR.data::grch37_lymphoma_genes_bed)
+#' 
+#' these_samples_metadata = get_gambl_metadata() %>% dplyr::filter(pathology=="BL")
+#' 
+#' # basic usage with 500 approximately equal-sized bins across the genome
+#' cn_matrix = get_cn_states(strategy="auto_split",
+#'                           n_bins_split=500)
 #'
+#' # get the CN state of every lymphoma gene instead
+#' 
+#' these_samples_metadata = get_gambl_metadata() %>% dplyr::filter(pathology=="BL")
 #'
 get_cn_states = function(strategy,
                          regions,
@@ -33,11 +40,17 @@ get_cn_states = function(strategy,
                          missing_data_as_diploid = FALSE,
                          adjust_for_ploidy=FALSE,
                          projection="grch37"){
-
-  these_samples_metadata = dplyr::filter(these_samples_metadata,seq_type!="mrna")
-  seg_data = get_cn_segments(these_samples_metadata = these_samples_metadata,projection = projection)
+  if(!missing(these_samples_metadata)){
+    these_samples_metadata = dplyr::filter(these_samples_metadata,seq_type!="mrna")  
+    seg_data = get_cn_segments(these_samples_metadata = these_samples_metadata,projection = projection)
+  }else{
+    seg_data = get_cn_segments(projection = projection)
+  }
+  
+  
   print(get_genome_build(seg_data))
   if(missing(regions)){
+    print("IF")
     cn_matrix = segmented_data_to_cn_matrix(strategy=strategy,
                                             seg_data=seg_data,
                                             these_samples_metadata = these_samples_metadata,
@@ -47,6 +60,7 @@ get_cn_states = function(strategy,
                                             adjust_for_ploidy = adjust_for_ploidy,
                                             genome_build = projection)
   }else{
+    print("ELSE")
     cn_matrix = segmented_data_to_cn_matrix(strategy=strategy,
                                             seg_data=seg_data,
                                             regions=regions,
