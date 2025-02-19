47af3f581590a7fbed2d4e08c4c1557c8b18db34
rdmorin <rdmorin@sfu.ca>
2025-02-18 18:56:31 -0800

    cleanup

--- R/get_excluded_samples.R
+++ R/get_excluded_samples.R
@@ -1,10 +1,14 @@
 #' @title Get Excluded Samples.
 #'
-#' @description Exclude samples that have been excluded from certain analyses and drop from merges.
+#' @description Exclude samples that have been excluded from certain
+#' analyses and drop from merges.
 #'
-#' @details Specify the tool or pipeline responsible for generating the files with `tool_name` and this function will return a vector of excluded sample IDs.
+#' @details Specify the tool or pipeline responsible for generating the
+#' files with
+#' `tool_name` and this function will return a vector of excluded sample IDs.
 #'
-#' @param tool_name The tool or pipeline that generated the files (should be the same for all). Default is "slms-3".
+#' @param tool_name The tool or pipeline that generated the files
+#' (should be the same for all). Default is "slms-3".
 #'
 #' @return A vector of sample IDs.
 #'
@@ -12,20 +16,22 @@
 #' @export
 #'
 #' @examples
-#' excluded_samp = get_excluded_samples()
+#' excluded_samp <- get_excluded_samples()
 #' @keywords internal
-get_excluded_samples = function(tool_name = "slms-3"){
-  base = GAMBLR.helpers::check_config_value(config::get("repo_base"))
+get_excluded_samples <- function(tool_name = "slms-3") {
+  base <- GAMBLR.helpers::check_config_value(config::get("repo_base"))
 
-  #check for missingness
-  path = paste0(base,"config/exclude.tsv")
-  if(!file.exists(path)){
+  # check for missingness
+  path <- paste0(base, "config/exclude.tsv")
+  if (!file.exists(path)) {
     print(paste("missing: ", path))
-    message("Have you cloned the GAMBL repo and added the path to this directory under the local section of your config?")
+    message("Have you cloned the GAMBL repo and added the path to
+    this directory under the local section of your config?")
   }
 
-  excluded_df = suppressMessages(read_tsv(paste0(base,"config/exclude.tsv")))
-  excluded_samples = dplyr::filter(excluded_df, pipeline_exclude == tool_name) %>%
+  excluded_df <- suppressMessages(read_tsv(paste0(base, "config/exclude.tsv")))
+  excluded_samples <- dplyr::filter(excluded_df,
+                                    pipeline_exclude == tool_name) %>%
     pull(sample_id)
 
   return(excluded_samples)
