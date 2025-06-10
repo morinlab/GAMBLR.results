6d8110384324113bb5fe6d21b03e2a0a827b6b8b
gillissierra <gillissierra@gmail.com>
2025-06-10 11:47:37 -0700

    update get_ssm_by_regions

--- R/get_ssm_by_regions.R
+++ R/get_ssm_by_regions.R
@@ -8,33 +8,47 @@
 #' [GAMBLR.results::get_coding_ssm_status], [GAMBLR.results::get_ssm_by_sample],
 #' [GAMBLR.results::get_ssm_by_samples], [GAMBLR.results::get_ssm_by_region]
 #'
-#' @param regions_list Either provide a vector of regions in the chr:start-end format OR.
+#' @param regions_list Either provide a vector of regions in the chr:start-end format OR
 #' @param regions_bed Better yet, provide a bed file with the coordinates you want to retrieve.
-#' @param these_sample_ids Optional, a vector of multiple sample_id (or a single sample ID as a string) that you want results for.
-#' @param these_samples_metadata Optional, a metadata table (with sample IDs in a column) to subset the return to.
-#' @param streamlined If TRUE (default), only 3 columns will be kept in the maf (start, sample_id and region name). 
-#' To return more columns, set this parameter to FALSE, see `basic_column` for more info. 
-#' Note, if this parameter is TRUE, the function will disregard anything specified with `basic_columns`.
+#' @param these_samples_metadata Optional metadata table.
+#'  If provided, it will return SSM calls for the only the samples in the metadata table.
+#'  Otherwise it will use all samples from `get_gambl_metadata()` of the appropriate seq_types.
 #' @param maf_data Use an already loaded MAF data frame.
-#' @param use_name_column If your bed-format data frame has a name column (must be named "name") these can be used to name your regions.
-#' @param from_indexed_flatfile Set to TRUE to avoid using the database and instead rely on flatfiles (only works for streamlined data, not full MAF details).
-#' @param mode Only works with indexed flatfiles. Accepts 2 options of "slms-3" and "strelka2" to indicate which variant caller to use. Default is "slms-3".
-#' @param augmented default: TRUE. Set to FALSE if you instead want the original MAF from each sample for multi-sample patients instead of the augmented MAF
-#' @param this_seq_type The seq_type you want back, default is genome.
-#' @param projection Obtain variants projected to this reference (one of grch37 or hg38).
-#' @param min_read_support Only returns variants with at least this many reads in t_alt_count (for cleaning up augmented MAFs).
-#' @param basic_columns Parameter to be used when streamlined is FALSE. 
-#' Set this parameter to TRUE for returning a maf with standard 45 columns, set to FALSE to keep all 116 maf columns in the returned object. 
-#' To return all 116 maf columns, set this parameter to FALSE.
+#'  If you would like all columns of this input maf returned,
+#'  set `streamlined = FALSE` and `basic_columns = FALSE`.
+#'  Otherwise the first 45 columns will be returned.
+#' @param streamlined If TRUE (default), only 3 columns will be returned:
+#'  start, sample_id, and region in the format "chr:start-end".
+#'  Note: if this parameter is TRUE, the function will disregard anything specified with `basic_columns`.
+#' @param use_name_column If TRUE and your bed-format data frame has a name column 
+#'  (must be named "name") these can be used to name your regions. To be used with streamlined = TRUE. Default: FALSE.
+#' @param basic_columns Parameter to be used when streamlined is FALSE.
+#'  Set this parameter to TRUE to return a MAF with the standard 45 columns.
+#'  Set to FALSE to return a MAF with all columns (116).
+#'  If you provided `maf_data` with more than 45 columns, set to FALSE to return all columns of
+#'  `maf_data`, otherwise it will return the first 45.
+#' @param tool_name Accepts either "slms-3" (default) or "strelka2"
+#'  (forces `streamlined=TRUE`) to indicate which variant caller to use. Note: strelka2 will force
+#'  `augmented=FALSE` as data is not available in that case. Formerly called "mode".
+#' @param augmented Default: TRUE. Setting to FALSE will subset from a pre-merged MAF.
+#'  Obtaining variants from the original MAFs for each sample would not be computationally efficient here.
+#'  Provide to `maf_data` the results of `get_ssm_by_samples` if those are needed.
+#' @param projection Obtain variants projected to this reference, one of grch37 (default) or hg38.
+#' @param min_read_support Only returns variants with at least this many reads in t_alt_count
+#'  (for cleaning up augmented MAFs). Default: 3.
 #' @param verbose Boolean parameter set to FALSE per default.
+#' @param this_seq_type Deprecated. Inferred from these_samples_metadata
+#' @param these_sample_ids Deprecated. Inferred from these_samples_metadata
 #'
-#' @return Returns a data frame of variants in MAF-like format.
+#' @return Returns a data frame of variants in 3 column format or in MAF-like format (one row per mutation).
 #'
 #' @import tibble dplyr tidyr GAMBLR.utils parallel
 #' @export
 #'
 #' @examples
 #' 
+#' # Adds column `name` to the bed-format dataframe
+#' # by combining "gene" and "region" values sep by "-"
 #' regions_bed = GAMBLR.utils::create_bed_data(
 #'    GAMBLR.data::grch37_ashm_regions,
 #'    fix_names = "concat",
@@ -55,26 +69,46 @@
 #'
 get_ssm_by_regions = function(regions_list,
                               regions_bed,
-                              these_sample_ids = NULL,
-                              these_samples_metadata = NULL,
-                              streamlined = TRUE,
-                              maf_data = maf_data,
+                              these_samples_metadata,
+                              maf_data,
                               use_name_column = FALSE,
-                              from_indexed_flatfile = TRUE,
-                              mode = "slms-3",
+                              streamlined = FALSE,
+                              basic_columns = TRUE,
+                              tool_name = "slms-3",
                               augmented = TRUE,
-                              this_seq_type = "genome",
                               projection = "grch37",
-                              min_read_support = 4,
-                              basic_columns = FALSE,
-                              verbose = FALSE){
-  genome_build = NULL
-  if(missing(these_samples_metadata)){
-    stop("these_samples_metadata is required")
+                              min_read_support = 3,
+                              verbose = FALSE,
+                              these_sample_ids,
+                              this_seq_type){
+  if(!missing(this_seq_type) | !missing(these_sample_ids)){
+    stop("this_seq_type and these_sample_ids are deprecated. Use these_samples_metadata instead")
   }
-  #ensure we only process the seq_type specified
-  these_samples_metadata = dplyr::filter(these_samples_metadata,
-    seq_type == this_seq_type)
+
+  if(!projection %in% c("grch37", "hg38")){
+    stop("projection must be either grch37 or hg38")
+  }
+  if(length(tool_name) != 1){
+    stop("tool_name can only be a single value, either slms-3 or strelka2")
+  }else if(!tool_name %in% c("slms-3", "strelka2")){
+    stop("tool_name must be either slms-3 or strelka2")
+  }  
+
+  # Also done in get_ssm_by_region, but this allows passing a df 
+  # for these_sample_metadata and avoidng a bunch of output messages
+  to_exclude = get_excluded_samples(tool_name)
+  if(missing(these_samples_metadata)){
+    # kept for legacy, assumes user provided these_sample_ids
+    message("CAUTION! these_samples_metadata was not provided. Using all of get_gambl_metadata().")
+    these_samples_metadata = get_gambl_metadata() %>%
+      dplyr::filter(seq_type!="mrna") %>%
+      dplyr::filter(!sample_id %in% to_exclude)
+  }else{
+    #drop unsupported seq_type and samples to exclude
+    these_samples_metadata = dplyr::filter(these_samples_metadata,seq_type!="mrna") %>%
+        dplyr::filter(!sample_id %in% to_exclude)
+  }
+
   if(streamlined){
     message("Streamlined is set to TRUE, this function will disregard anything specified with basic_columns")
     message("To return a MAF with standard 45 columns, set streamlined = FALSE and basic_columns = TRUE")
@@ -84,6 +118,7 @@ get_ssm_by_regions = function(regions_list,
     paste0(x[1], ":", as.numeric(x[2]), "-", as.numeric(x[3]))
   }
 
+  genome_build = NULL
   if(missing(regions_list)){
     if(!missing(regions_bed)){
       genome_build = check_get_projection(list(this_bed=regions_bed),
@@ -100,6 +135,7 @@ get_ssm_by_regions = function(regions_list,
   if(verbose){
     print(regions)
   }
+
   if(missing(maf_data)){
     #projection is required if the MAF was not provided
     if(is.null(genome_build)){
@@ -113,27 +149,30 @@ get_ssm_by_regions = function(regions_list,
       region = x,          
       these_samples_metadata = these_samples_metadata,
       streamlined = streamlined,
-      from_indexed_flatfile = from_indexed_flatfile,
-      mode = mode,
+      basic_columns = basic_columns,
+      tool_name = tool_name,
       augmented = augmented,
-      this_seq_type = this_seq_type,
       projection = genome_build,
-      basic_columns = basic_columns
+      min_read_support = min_read_support,
+      verbose = verbose
       )},
       mc.cores = 12)
   }else{
     region_mafs = parallel::mclapply(regions, function(x){get_ssm_by_region(
       region = x,
       these_samples_metadata = these_samples_metadata,
-      this_seq_type = this_seq_type,
-      streamlined = streamlined,
       maf_data = maf_data,
-      from_indexed_flatfile = from_indexed_flatfile,
-      mode = mode,
-      basic_columns = basic_columns
+      streamlined = streamlined,
+      basic_columns = basic_columns,
+      tool_name = tool_name,
+      augmented = augmented,
+      projection = genome_build,
+      min_read_support = min_read_support,
+      verbose = verbose
       )},
       mc.cores = 12)
   }
+
   if(!use_name_column){
     rn = regions
   }else{
@@ -153,5 +192,5 @@ get_ssm_by_regions = function(regions_list,
     region_mafs = GAMBLR.utils::create_maf_data(region_mafs, genome_build=genome_build)
     return(region_mafs)
   }
-  
+
 }
