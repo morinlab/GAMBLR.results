69eb3e750b043264af493568f2eb3ff5cdc3fc6d
gillissierra <gillissierra@gmail.com>
2025-07-31 09:15:46 -0700

    fix check of regions_list

--- R/get_ssm_by_regions.R
+++ R/get_ssm_by_regions.R
@@ -37,8 +37,8 @@
 #' @param min_read_support Only returns variants with at least this many reads in t_alt_count
 #'  (for cleaning up augmented MAFs). Default: 3.
 #' @param verbose Boolean parameter set to FALSE per default.
-#' @param this_seq_type Deprecated. Inferred from these_samples_metadata
-#' @param these_sample_ids Deprecated. Inferred from these_samples_metadata
+#' @param this_seq_type Deprecated. Inferred from these_samples_metadata.
+#' @param these_sample_ids Deprecated. Inferred from these_samples_metadata.
 #'
 #' @return Returns a data frame of variants in 3 column format or in MAF-like format (one row per mutation).
 #'
@@ -126,7 +126,8 @@ get_ssm_by_regions = function(regions_list,
                                         custom_error = "Please specify a projection that matches the genome build of regions_bed")
 
     regions = apply(regions_bed, 1, bed2region)
-  }else if(!missing(regions_list) & (sum((c("bed_data", "genomic_data", "data.frame") %in% class(ashm_bed))) >= 1)){
+  }else if(!missing(regions_list) & (sum((c("bed_data", "genomic_data", "data.frame") %in% class(regions_list))) >= 1)){
+    # bed data was given but not specified with regions_bed, so it is saved in parameter regions_list
     genome_build = check_get_projection(list(this_bed=regions_list),
                                         projection,
                                         custom_error = "Please specify a projection that matches the genome build of regions_bed")
