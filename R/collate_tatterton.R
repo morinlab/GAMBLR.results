dcc07fcfc0e258b13dce767027522717a05ee845
gbanco <giulianobanco4@gmail.com>
2026-07-14 16:48:53 -0700

    Change Tatterton file path to tsv

--- R/collate_tatterton.R
+++ R/collate_tatterton.R
@@ -43,7 +43,7 @@ collate_tatterton = function(
         config::get("project_base")
     )
 
-    tatterton_table <- "icgc_dart/exome_data/tatterton_s1b.csv"
+    tatterton_table <- "icgc_dart/exome_data/tatterton_s1b.tsv"
 
     full_path <- paste0(base, tatterton_table)
 
